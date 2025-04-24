68e5b9d0d981723c44dbf169634c7b8f618687c2
jeankonan748 <jean.konan@epitech.eu>
2025-04-24 10:30:28 +0000

    ADD config api

--- bootcamp_django/Project_Kyc/urls.py
+++ bootcamp_django/Project_Kyc/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -26,7 +28,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),  # Inclut les URLs définies dans users/urls.py
-    path('api/pieces/', include('identites.urls')),  # Inclut les URLs définies dans identites/urls.py
+    path('api/pieces/', include('users.urls')),  # Inclut les URLs définies dans identites/urls.py
 ]
 
 # Pour servir les fichiers médias pendant le développement
